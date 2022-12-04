5ea6559bd11e709b71a8f15dbb9c072e16828d9b
Gedas Valuntis <gedas.valuntis@sis.lt>
2022-12-04 11:48:45 +0200

    Day 4

---
+++ Advent-of-Code-2022/2022/Day4_2022.cs
@@ -0,0 +1,53 @@
+﻿namespace Advent_of_Code_2022._2022;
+
+public static class Day4_2022
+{
+    public static void Run(string testInputPath, string challengeInputPath)
+    {
+        Solve(File.ReadAllLines(testInputPath));
+        Solve(File.ReadAllLines(challengeInputPath));
+    }
+
+    private static void Solve(string[] lines)
+    {
+        var fullyContainedSum = 0;
+        var overlappingSum = 0;
+        foreach (var line in lines)
+        {
+            var pairs = line.Split(',');
+            var a = Section.Parse(pairs[0]);
+            var b = Section.Parse(pairs[1]);
+            
+            fullyContainedSum += IsFullyContained(a, b) ? 1 : 0;
+            overlappingSum += IsOverlapping(a, b) ? 1 : 0;
+        }
+
+        Console.WriteLine($"FullyContained: {fullyContainedSum} pairs");
+        Console.WriteLine($"Overlapping: {overlappingSum} pairs");
+    }
+
+    private static bool IsFullyContained(Section a, Section b)
+    {
+        return
+            // b in a?
+            b.Start >= a.Start && b.Start <= a.End && b.End <= a.End ||
+            // a in b?
+            a.Start >= b.Start && a.Start <= b.End && a.End <= b.End;
+    }
+
+    private static bool IsOverlapping(Section a, Section b)
+    {
+        return
+            a.Start <= b.End && b.Start <= a.End;
+    }
+    
+    private record struct Section(int Start, int End)
+    {
+        public static Section Parse(string input)
+        {
+            var splits = input.Split("-");
+            if (splits.Length != 2) throw new IndexOutOfRangeException($"{input} could not be parsed");
+            return new Section(int.Parse(splits[0]), int.Parse(splits[1]));
+        }
+    }
+}
--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -6,8 +6,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2022\Inputs\Test\testday3.txt";
-        var input = @"2022\Inputs\day3.txt";
-        Day3_2022.Run(testInput, input);
+        var testInput = @"2022\Inputs\Test\testday4.txt";
+        var input = @"2022\Inputs\day4.txt";
+        Day4_2022.Run(testInput, input);
     }
 }
