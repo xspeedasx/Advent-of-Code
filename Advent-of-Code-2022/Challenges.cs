c0cf8b609c78329db8957205baf138c426855397
Gedas Valuntis <gedasv@unifysquare.com>
2022-12-06 08:04:10 +0200

    Day 6

--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -6,8 +6,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2022\Inputs\Test\testday5.txt";
-        var input = @"2022\Inputs\day5.txt";
-        Day5_2022.Run(testInput, input);
+        var testInput = @"2022\Inputs\Test\testday6.txt";
+        var input = @"2022\Inputs\day6.txt";
+        Day6_2022.Run(testInput, input);
     }
 }
