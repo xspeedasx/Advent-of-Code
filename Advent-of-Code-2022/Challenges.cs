bfee18906f5b333b27a1c374308a35fe5134c328
Gedas <xspeedasx@gmail.com>
2022-12-09 08:17:24 +0200

    Day 9

--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -6,8 +6,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2022\Inputs\Test\testday8.txt";
-        var input = @"2022\Inputs\day8.txt";
-        Day8_2022.Run(testInput, input);
+        var testInput = @"2022\Inputs\Test\testday9.txt";
+        var input = @"2022\Inputs\day9.txt";
+        Day9_2022.Run(testInput, input);
     }
 }
