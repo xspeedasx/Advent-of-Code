c33a07b32197ec85728fcfdf1998d20796bb3515
Gedas <xspeedasx@gmail.com>
2023-12-01 06:23:10 +0200

    2023 preparations

---
+++ Advent-of-Code-2022/2023/Day1_2023.cs
@@ -0,0 +1,15 @@
+﻿namespace Advent_of_Code_2022._2023;
+
+public static class Day1_2023
+{
+    public static void Run(string testInputPath, string challengeInputPath)
+    {
+        Solve(File.ReadAllLines(testInputPath));
+        Solve(File.ReadAllLines(challengeInputPath));
+    }
+
+    private static void Solve(string[] lines)
+    {
+        Console.WriteLine("Hello, world!");
+    }
+}
--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -1,4 +1,5 @@
 ﻿using Advent_of_Code_2022._2022;
+using Advent_of_Code_2022._2023;
 
 namespace Advent_of_Code_2022;
 
@@ -6,8 +7,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2022\Inputs\Test\testday17.txt";
-        var input = @"2022\Inputs\day17.txt";
-        Day17_2022.Run(testInput, input);
+        var testInput = @"2023\Inputs\Test\testday1.txt";
+        var input = @"2023\Inputs\day1.txt";
+        Day1_2023.Run(testInput, input);
     }
 }
