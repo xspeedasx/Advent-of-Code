34147ee1643062811a040e0a96a1924a038dbe08
GedasValuntis <gedas.valuntis@twoday.com>
2023-12-01 08:00:22 +0200

    clean up code

--- Advent-of-Code-2022/2023/Day1_2023.cs
+++ Advent-of-Code-2022/2023/Day1_2023.cs
@@ -9,9 +9,10 @@ public static class Day1_2023
         // part 1
         Solve(File.ReadAllLines(testInputPath));
         Solve(File.ReadAllLines(challengeInputPath));
-        
+
         // part 2
-        var testpath2 = Path.Combine(Path.GetDirectoryName(testInputPath)!,
+        var testpath2 = Path.Combine(
+            Path.GetDirectoryName(testInputPath)!,
             Path.GetFileNameWithoutExtension(testInputPath) + "_2.txt");
         Solve2(File.ReadAllLines(testpath2));
         Solve2(File.ReadAllLines(challengeInputPath));
@@ -36,7 +37,7 @@ public static class Day1_2023
     private static Dictionary<int, int> GetTokens(string line)
     {
         var tokens = new Dictionary<int, int>();
-        for (int i = 0; i < line.Length; i++)
+        for (var i = 0; i < line.Length; i++)
         {
             var token = MatchKey(line[i..]);
             if (token != null)
@@ -46,42 +47,25 @@ public static class Day1_2023
         }
 
         return tokens;
-        
+
         int? MatchKey(string s)
         {
-            if (Char.IsDigit(s[0]))
+            if (char.IsDigit(s[0]))
             {
                 return int.Parse(s[0] + "");
             }
 
-            if(s[..Math.Min(3, s.Length)].Equals("one", StringComparison.OrdinalIgnoreCase)) return 1;
-            if(s[..Math.Min(3, s.Length)].Equals("two", StringComparison.OrdinalIgnoreCase)) return 2;
-            if(s[..Math.Min(5, s.Length)].Equals("three", StringComparison.OrdinalIgnoreCase)) return 3;
-            if(s[..Math.Min(4, s.Length)].Equals("four", StringComparison.OrdinalIgnoreCase)) return 4;
-            if(s[..Math.Min(4, s.Length)].Equals("five", StringComparison.OrdinalIgnoreCase)) return 5;
-            if(s[..Math.Min(3, s.Length)].Equals("six", StringComparison.OrdinalIgnoreCase)) return 6;
-            if(s[..Math.Min(5, s.Length)].Equals("seven", StringComparison.OrdinalIgnoreCase)) return 7;
-            if(s[..Math.Min(5, s.Length)].Equals("eight", StringComparison.OrdinalIgnoreCase)) return 8;
-            if(s[..Math.Min(4, s.Length)].Equals("nine", StringComparison.OrdinalIgnoreCase)) return 9;
+            if (s[..Math.Min(3, s.Length)].Equals("one", StringComparison.OrdinalIgnoreCase)) return 1;
+            if (s[..Math.Min(3, s.Length)].Equals("two", StringComparison.OrdinalIgnoreCase)) return 2;
+            if (s[..Math.Min(5, s.Length)].Equals("three", StringComparison.OrdinalIgnoreCase)) return 3;
+            if (s[..Math.Min(4, s.Length)].Equals("four", StringComparison.OrdinalIgnoreCase)) return 4;
+            if (s[..Math.Min(4, s.Length)].Equals("five", StringComparison.OrdinalIgnoreCase)) return 5;
+            if (s[..Math.Min(3, s.Length)].Equals("six", StringComparison.OrdinalIgnoreCase)) return 6;
+            if (s[..Math.Min(5, s.Length)].Equals("seven", StringComparison.OrdinalIgnoreCase)) return 7;
+            if (s[..Math.Min(5, s.Length)].Equals("eight", StringComparison.OrdinalIgnoreCase)) return 8;
+            if (s[..Math.Min(4, s.Length)].Equals("nine", StringComparison.OrdinalIgnoreCase)) return 9;
             return null;
         }
-    }
-
-    private static int ParseMatch(string m)
-    {
-        return m switch
-        {
-            "one" => 1,
-            "two" => 2,
-            "three" => 3,
-            "four" => 4,
-            "five" => 5,
-            "six" => 6,
-            "seven" => 7,
-            "eight" => 8,
-            "nine" => 9,
-            _ => int.Parse(m)
-        };
     }
 
     private static void Solve(string[] lines)
