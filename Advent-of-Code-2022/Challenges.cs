bcc119c4e40c2ad8e02505445f9a20a441b8a512
Gedas <xspeedasx@gmail.com>
2023-12-07 08:33:38 +0200

    Day 7

--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -7,8 +7,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2023\Inputs\Test\testday6.txt";
-        var input = @"2023\Inputs\day6.txt";
-        Day6_2023.Run(testInput, input);
+        var testInput = @"2023\Inputs\Test\testday7.txt";
+        var input = @"2023\Inputs\day7.txt";
+        Day7_2023.Run(testInput, input);
     }
 }
