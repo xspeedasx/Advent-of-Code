2e96c3b199197a8469988c30556a7493968b9abc
Gedas <xspeedasx@gmail.com>
2023-12-08 08:17:18 +0200

    day 8 stupid solution, too long. data analysis for smart solution start.

--- Advent-of-Code-2022/Challenges.cs
+++ Advent-of-Code-2022/Challenges.cs
@@ -7,8 +7,8 @@ public static class Challenges
 {
     public static void Run()
     {
-        var testInput = @"2023\Inputs\Test\testday7.txt";
-        var input = @"2023\Inputs\day7.txt";
-        Day7_2023.Run(testInput, input);
+        var testInput = @"2023\Inputs\Test\testday8.txt";
+        var input = @"2023\Inputs\day8.txt";
+        Day8_2023.Run(testInput, input);
     }
 }
