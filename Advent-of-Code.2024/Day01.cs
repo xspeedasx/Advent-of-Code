608a078e1382f08631417632b1016df2677615ab
Gedas <xspeedasx@gmail.com>
2024-11-30 17:26:01 +0200

    2024 setup

---
+++ Advent-of-Code.2024/Day01.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Advent_of_Code._2024;
+
+public class Day01
+{
+    private readonly string _input = File.ReadAllText(@"Inputs\Day01.txt");
+
+    [Fact]
+    public void Part1Test()
+    {
+        Assert.Equal("test", _input);
+    }
+}
